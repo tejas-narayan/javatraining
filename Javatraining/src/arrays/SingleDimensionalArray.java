2f41f836d56d5f6f884dc536a1ba2896f550bba5
tejas <gowdatejas251@gmail.com>
2021-04-21 20:53:27 +0530

    commit

---
+++ Javatraining/src/arrays/SingleDimensionalArray.java
@@ -0,0 +1,25 @@
+package arrays;
+
+public class SingleDimensionalArray {
+
+	public static void main(String[] args) {
+
+
+		String names[] = new String[4] ;
+		names[0] = "Asha";
+		names[1] = "Namratha";
+		names[2] = "Sanjay";
+		names[3] = "Tejas";
+		
+		for(int i=0;i<names.length;i++) {
+
+		System.out.println(names[i]);
+	}
+		
+		char[] ch = {'a','b','c','d'};
+		for(int i = 0;i<ch.length;i++) {
+		System.out.println(ch[i]);
+
+}
+}
+}
